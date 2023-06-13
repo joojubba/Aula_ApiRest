f4c13fa61a1a65b456901df44aad0dd1279dd64d
Joanna Ramalho <joanna-karla@hotmail.com>
2023-06-13 17:24:31 -0300

    Aula ApiRest

--- WebApplication1/WebApplication1/Controllers/PrimeiraController.cs
+++ WebApplication1/WebApplication1/Controllers/PrimeiraController.cs
@@ -3,8 +3,7 @@
 namespace WebApplication1.Controllers
 {
     [ApiController]
-    [Route("[controller]")] // fica Primeira, se fosse
-    //sem o conchete seria apenas a palavra controller
+    [Route("[controller]")] 
     public class PrimeiraController : ControllerBase
     {
         [HttpGet("primeiro")]
